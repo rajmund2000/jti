201c9023cf51fd6f3b5742d039bc0835cc08f69b
lukasz.jarkowski <jarkowski.lukasz@gmail.com>
2025-10-28 23:12:05 +0100

    extend timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,12 +26,14 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://www.ploom.co.uk/en',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    actionTimeout:30000,
+    navigationTimeout:45000,
   },
-
+  timeout:120000,
   /* Configure projects for major browsers */
   projects: [
     {
